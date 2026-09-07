1677d57a06988b28079ba2d549bfce6086afdd9b
joschonarth <joschonarth@gmail.com>
2026-09-07 19:26:20 -0300

    feat: update sidebar component to include router links for navigation

--- frontend/src/app/shared/components/sidebar/sidebar.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AuthManager } from '../../../core/services/auth-manager';
 import { InitialsPipe } from '../../pipes/initials-pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [InitialsPipe],
-  templateUrl: './sidebar.html'
+  imports: [InitialsPipe, RouterLink],
+  templateUrl: './sidebar.html',
 })
 export class Sidebar {
   readonly _authManager = inject(AuthManager);
