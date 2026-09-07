b0487b93e8a7c55e5836a32e5e806437e9fed1e6
joschonarth <joschonarth@gmail.com>
2026-09-07 07:26:39 -0300

    feat: add not found route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,4 +6,9 @@ export const routes: Routes = [
     loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
     title: 'Login',
   },
+  {
+    path: '**',
+    loadComponent: () => import('./features/not-found/not-found').then((m) => m.NotFound),
+    title: 'Página não encontrada',
+  },
 ];
