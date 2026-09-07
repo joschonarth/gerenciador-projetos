75ce8e2afe1d40b5ca75ffc715c62804b31f1ff7
joschonarth <joschonarth@gmail.com>
2026-09-07 19:19:22 -0300

    feat: implement auth guard for route protection

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './shared/layouts/main-layout/main-layout';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
--- frontend/src/app/core/guards/auth-guard.ts
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -1,2 +1,15 @@
+import { CanMatchFn, RedirectCommand, Router } from '@angular/router';
+import { AuthManager } from '../services/auth-manager';
+import { inject } from '@angular/core';
 
-export const authGuard = null;
+export const authGuard: CanMatchFn = () => {
+  const authManager = inject(AuthManager);
+  const router = inject(Router);
+
+  if (authManager.isAuthenticated()) {
+    return true;
+  }
+
+  const urlTree = router.createUrlTree(['/login']);
+  return new RedirectCommand(urlTree, { replaceUrl: true });
+};
