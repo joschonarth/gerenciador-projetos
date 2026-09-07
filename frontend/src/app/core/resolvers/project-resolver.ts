36384bcd6621e49d1850423e9672c4b92035db3a
joschonarth <joschonarth@gmail.com>
2026-09-07 19:44:57 -0300

    feat: implement project resolver for route data fetching

--- frontend/src/app/core/resolvers/project-resolver.ts
+++ frontend/src/app/core/resolvers/project-resolver.ts
@@ -1,2 +1,11 @@
+import { ResolveFn } from '@angular/router';
+import { IProject } from '../models';
+import { inject } from '@angular/core';
+import { ProjectApi } from '../services/project-api';
 
-export const projectResolver = null;
+export const projectResolver: ResolveFn<IProject> = (route) => {
+  const projectApi = inject(ProjectApi);
+  const projectId = route.paramMap.get('projectId')!;
+
+  return projectApi.getById(projectId);
+};
--- frontend/src/app/features/project/project.routes.ts
+++ frontend/src/app/features/project/project.routes.ts
@@ -1,27 +1,34 @@
 import { Routes } from '@angular/router';
+import { projectResolver } from '../../core/resolvers/project-resolver';
 
 export const PROJECT_ROUTES: Routes = [
-  { path: '', redirectTo: 'board', pathMatch: 'full' },
   {
-    path: 'board',
-    loadComponent: () => import('./board/board').then((m) => m.Board),
-    title: 'Board',
-  },
-  {
-    path: 'backlog',
-    loadComponent: () => import('./backlog/backlog').then((m) => m.Backlog),
-    title: 'Backlog',
-  },
-  {
-    path: 'settings',
-    loadComponent: () =>
-      import('./project-settings/project-settings').then((m) => m.ProjectSettings),
-    title: 'Configurações do Projeto',
-  },
-  {
-    path: 'task/:taskId',
-    outlet: 'detail',
-    loadComponent: () => import('./task-detail/task-detail').then((m) => m.TaskDetail),
-    title: 'Detalhes da Task',
+    path: '',
+    resolve: { project: projectResolver },
+    children: [
+      { path: '', redirectTo: 'board', pathMatch: 'full' },
+      {
+        path: 'board',
+        loadComponent: () => import('./board/board').then((m) => m.Board),
+        title: 'Board',
+      },
+      {
+        path: 'backlog',
+        loadComponent: () => import('./backlog/backlog').then((m) => m.Backlog),
+        title: 'Backlog',
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./project-settings/project-settings').then((m) => m.ProjectSettings),
+        title: 'Configurações do Projeto',
+      },
+      {
+        path: 'task/:taskId',
+        outlet: 'detail',
+        loadComponent: () => import('./task-detail/task-detail').then((m) => m.TaskDetail),
+        title: 'Detalhes da Task',
+      },
+    ],
   },
 ];
