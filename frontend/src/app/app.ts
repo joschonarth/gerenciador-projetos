9f75c8c889769fc2e2b1a5ed5b3c43219ddb2432
joschonarth <joschonarth@gmail.com>
2026-09-07 21:19:03 -0300

    feat: integrate loading bar component and enhance loading state management

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingBar } from './shared/components/loading-bar/loading-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, LoadingBar],
+  template: `
+    <app-loading-bar />
+    <router-outlet />
+  `,
   styles: [],
 })
 export class App {}
--- frontend/src/app/shared/components/loading-bar/loading-bar.ts
+++ frontend/src/app/shared/components/loading-bar/loading-bar.ts
@@ -1,14 +1,39 @@
-import { Component, OnDestroy, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-loading-bar',
   templateUrl: './loading-bar.html',
-  styleUrls: ['./loading-bar.scss']
+  styleUrls: ['./loading-bar.scss'],
 })
-export class LoadingBar implements OnDestroy {
-  isLoading = signal(false);
+export class LoadingBar implements OnInit, OnDestroy {
+  isLoading = signal(true);
+  routerSub!: Subscription;
+
+  private readonly _router = inject(Router);
+
+  ngOnInit() {
+    this.routerSub = this._router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isLoading.set(true);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        setTimeout(() => this.isLoading.set(false), 300);
+      }
+    });
+  }
 
   ngOnDestroy(): void {
-
+    this.routerSub?.unsubscribe();
   }
 }
