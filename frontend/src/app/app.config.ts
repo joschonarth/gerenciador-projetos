a57c4191acc1ffd1395972965ebe25621034254a
joschonarth <joschonarth@gmail.com>
2026-09-07 21:37:21 -0300

    feat: add view transitions to router configuration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter, TitleStrategy, withPreloading } from '@angular/router';
+import { provideRouter, TitleStrategy, withPreloading, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes, withPreloading(CustomPreloadingStrategy)),
+    provideRouter(routes, withPreloading(CustomPreloadingStrategy), withViewTransitions()),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     {
       provide: TitleStrategy,
