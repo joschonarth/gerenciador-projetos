30313b001be09c3fea70691b2986f2a84244dbbd
joschonarth <joschonarth@gmail.com>
2026-09-07 21:35:53 -0300

    feat: implement custom title strategy for dynamic page titles

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,13 +3,14 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter, withPreloading } from '@angular/router';
+import { provideRouter, TitleStrategy, withPreloading } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
 import { errorInterceptor } from './core/interceptors/error-interceptor';
 import { CustomPreloadingStrategy } from './core/strategies/custom-preloading-strategy';
+import { CustomTitleStrategy } from './core/strategies/custom-title-strategy';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +18,9 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes, withPreloading(CustomPreloadingStrategy)),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    {
+      provide: TitleStrategy,
+      useClass: CustomTitleStrategy,
+    },
   ],
 };
--- frontend/src/app/core/strategies/custom-title-strategy.ts
+++ frontend/src/app/core/strategies/custom-title-strategy.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
-export class CustomTitleStrategy {
+export class CustomTitleStrategy extends TitleStrategy {
+  private readonly appName = 'Gestão de Projetos';
+  private readonly _title = inject(Title);
 
+  constructor() {
+    super();
+  }
+
+  override updateTitle(snapshot: RouterStateSnapshot): void {
+    const pageTitle = this.buildTitle(snapshot);
+
+    if (pageTitle) {
+      this._title.setTitle(`${pageTitle} | ${this.appName}`);
+    } else {
+      this._title.setTitle(this.appName);
+    }
+  }
 }
